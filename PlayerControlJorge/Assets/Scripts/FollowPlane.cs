583252b7363804be3ec389e0ab9278521bb009aa
Jorge <719727@student.dusd.net>
2023-02-27 11:40:22 -0800

    Scripted player

---
+++ PlayerControlJorge/Assets/Scripts/FollowPlane.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowPlane : MonoBehaviour
+{
+    public GameObject plane;
+    private Vector3 offset;
+    // Start is called before the first frame update
+    void Start()
+    {
+        offset = transform.position - plane.transform.position;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = plane.transform.position + offset;
+    }
+}
--- PlayerControlJorge/Assets/Scripts/Player.cs
+++ PlayerControlJorge/Assets/Scripts/Player.cs
@@ -4,15 +4,19 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float speed;
+    public float rotationSpeed;
+    public float verticalInput;
+   
 
     // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        //get the users vertical input
+        verticalInput = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+
+        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
     }
 }
